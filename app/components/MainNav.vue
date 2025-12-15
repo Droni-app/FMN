172833056b3a60065ed42039a69ac78824390f2d
Gustavo Barragan <dev@droni.co>
2025-12-15 12:35:42 -0500

    feat: init project

---
+++ app/components/MainNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-rose-50 p-3 shadow-md">
+    <div class="container mx-auto flex">
+      <NuxtLink to="/" class="mr-4">
+        <img src="@/assets/img/logo-FMN.webp" alt="Logo" class="h-12">
+      </NuxtLink>
+      <nav class="grow">
+        <ul class="flex justify-end">
+          <li v-for="item in menu" :key="item.name" class="mr-4">
+            <NuxtLink :to="item.link">{{ item.name }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+<script setup lang="ts">
+const menu = [
+  { name: 'Ejes', link: '/' },
+  { name: 'Asalvo', link: '/about' },
+  { name: 'Congreso', link: '/contact' },
+  { name: 'Talleres', link: '/contact' },
+  { name: 'Voluntariado', link: '/contact' },
+  { name: 'Noticias', link: '/contact' },
+  { name: 'Tienda', link: '/contact' },
+  { name: 'Donar', link: '/contact' },
+  { name: 'Transparencia', link: '/contact' },
+  { name: 'Contacto', link: '/contact' },
+];
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <MainNav />
     <h1>Welcome to My Website</h1>
   </div>
 </template>
